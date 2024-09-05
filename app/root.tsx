0c9f9bd10ebeb535018aa02398f31e94824ba703
Seth Davis <sethdavis512@gmail.com>
2024-09-05 08:47:14 -0500

    Upgrade to vite

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,8 @@
 import { Theme } from "@radix-ui/themes";
-import radixStyles from "@radix-ui/themes/styles.css";
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -13,13 +10,9 @@ import {
 } from "@remix-run/react";
 
 import { getUser } from "~/session.server";
-import stylesheet from "~/tailwind.css";
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
-  { rel: "stylesheet", href: radixStyles },
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+import "~/tailwind.css";
+import "@radix-ui/themes/styles.css";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   return json({ user: await getUser(request) });
@@ -39,7 +32,6 @@ export default function App() {
           <Outlet />
           <ScrollRestoration />
           <Scripts />
-          <LiveReload />
         </body>
       </Theme>
     </html>
